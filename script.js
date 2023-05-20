dec3f7fc6d89fc6b2d50275a0bfdf1697e9c9065
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-05-20 15:05:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const student = {
+  name: 'John Doe'
+};
+
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+}
+
+console.log(student.getKeys()); 
